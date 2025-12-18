8c7f3987f376dc14845e4a2c98d87baaedc6f42f
Manasa1901 <write2manasadevi@gmail.com>
2025-12-18 22:49:46 +0530

    Modified Orders

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { useCart } from "../context/CartContext";
 import { toast } from "react-toastify";
 
